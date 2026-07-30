4ab7cf83fe6ac8a6ce4b268c2b206a8d1b858300
Prequena-84 <pedrorequenarondon@hotmail.com>
2026-07-29 21:05:15 -0400

    [ADD]-0-module_transactions

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -1,26 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 
-//import { TransactionController } from './controllers/transaction.controller';
-//import { TransactionService } from './transaction.service';
+import { TransactionEntity } from './domain/transaction.entity';
+import { TransactionRepository } from './repositories/transaction.repository';
+import { TransactionController } from './interfaces/controller/transaction.controller';
 
 @Module({
   imports: [
-    // 1. Conexión GRPC al Ledger (Puerto 50051)
+    // 1. Registro de la Entidad en TypeORM
+    TypeOrmModule.forFeature([TransactionEntity]),
+
+    // 2. Conexión GRPC al Ledger (Puerto 50051)
     ClientsModule.register([
       {
         name: 'LEDGER_SERVICE',
         transport: Transport.GRPC,
         options: {
           package: 'ledger',
-          protoPath: join(__dirname, '../../proto/ledger_service.proto'),
+          protoPath: join(process.cwd(), 'src/proto/ledger_service.proto'), // Usamos process.cwd() como acordamos antes
           url: '[IP_ADDRESS]', // Usamos la IP del host para conectar con la Mac - 'localhost:5000',
         },
       },
     ]),
   ],
-  controllers: [/*TransactionController*/],
-  providers: [/*TransactionService*/],
+  controllers: [TransactionController],
+  providers: [TransactionRepository],
+  exports: [TransactionRepository], // Exportado por si otro módulo necesita consultarlo
 })
-export class TransactionModule {};
+export class TransactionModule { };
